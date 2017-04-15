288f6633456020079d3586384996149e3bc7173b
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-04-15 20:18:38 +0300

    Made cli prints for headlines similar width

--- rs-ycomb-hn-cli/src/cli.rs
+++ rs-ycomb-hn-cli/src/cli.rs
@@ -1,13 +1,20 @@
 use models::*;
 
-pub fn print_headline_with_author(item:&HnItem, index:&i32) {
+pub fn print_headline_with_author(item: &HnItem, index: &i32) {
     let s = create_headline_with_author(item, index).unwrap(); // Not handling errs
     println!("{}", s);
 }
 
-fn create_headline_with_author(item:&HnItem, index:&i32) -> Result<String,String> {
+fn create_headline_with_author(item: &HnItem, index: &i32) -> Result<String, String> {
     match item.title {
-        Some(ref title) => Ok(format!("[{}] {} ~{}", &index, &item.title.as_ref().unwrap(), &item.by)),
-        None => Err(String::from("Not headline"))
+        Some(ref title) => {
+            let s = format!("[{:3}] {:80} ~ {}",
+                            &index,
+                            &item.title.as_ref().unwrap(),
+                            &item.by);
+            Ok(s)
+
+        }
+        None => Err(String::from("Not headline")),
     }
 }
