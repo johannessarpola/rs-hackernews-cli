3f4f537a93a146b291cbeb74fd3ced5f8bf1f6fd
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-06-24 22:50:58 +0300

    Comment printing proto cli

--- rs-ycomb-hn-cli/src/cli.rs
+++ rs-ycomb-hn-cli/src/cli.rs
@@ -1,4 +1,5 @@
 use models::*;
+use std::iter::repeat;
 
 pub fn print_headline_with_author(item: &HnItem, index: &i32) {
     let s = create_headline_with_author(item, index).unwrap(); // Not handling errs
@@ -17,6 +18,27 @@ fn create_headline_with_author(item: &HnItem, index: &i32) -> Result<String, Str
         }
         None => Err(String::from("Not headline")),
     }
+}
+
+pub fn print_comments(items: &Vec<HnItem>) {
+    println!("Comments: \n");
+    for item in items {
+        println!("{}", create_comment_string(item, &1).unwrap()); // todo depth and error handling
+    }
+}
+
+fn create_comment_string(item:&HnItem, depth:&usize) -> Result<String, String> {
+    let depthStr = repeat("-").take(*depth).collect::<String>();
+    if(item.text.is_some()) {
+        return Ok(format!("{} {} ~ {} on {}", depthStr, item.text.as_ref().unwrap(), &item.by, &item.time))
+    }
+    else {
+        return Err(String::from("Could not get text for comment"))
+    }
+}
+
+pub fn no_comments_for(numb:&usize) {
+    println!("No comments for [{}]", numb);
 }
 
 pub fn print_filename_of_loaded_page(filen:&str, title:&str){
@@ -48,5 +70,22 @@ mod tests {
         assert!(s.contains("My YC app: Dropbox - Throw away your USB drive"));
         assert!(deserialized.by.len() != 0);
         assert!(deserialized.title.unwrap().len() != 0);
+    }
+
+    #[test]
+    fn create_comment_string_test() {
+        use std::fs::File;
+        use std::io::prelude::*;
+        use serde_json;
+        let mut contents = String::new();
+        File::open("res/test/children-item.json")
+            .and_then(|mut file| file.read_to_string(&mut contents))
+            .unwrap();
+        let deserialized: HnItem = serde_json::from_str(&contents).unwrap();
+        let commentStr = create_comment_string(&deserialized, &2).unwrap();
+        assert!(commentStr.contains("is not a valid concern. Unless you are planning"));
+        assert!(commentStr.contains("cholantesh"));
+        assert!(commentStr.contains("-- "));
+        
     }
 }
