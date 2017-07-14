1474ae3dde82b46fb876482104c47524e52b1387
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-07-14 20:11:39 +0300

    link formatting and fixed some bugs in regex

--- cli/src/formatting/tokens.rs
+++ cli/src/formatting/tokens.rs
@@ -18,17 +18,29 @@ fn replace_paragraphs(s: String) -> String {
 }
 
 fn replace_paragraphs_closing_tags(s: &str) -> String {
-    let closing_tags_with_whitespace = Regex::new(r"(\s*</p>\s*)").unwrap();
-    closing_tags_with_whitespace.replace_all(s, "").into_owned()
+    let re = Regex::new(r"( *</p> *)").unwrap();
+    re.replace_all(s, "").into_owned()
 }
 
 fn replace_paragraphs_opening_tags(s: &str) -> String {
-    let opening_tags_with_whitespace = Regex::new(r"(\s*<p>\s*)").unwrap();
-    opening_tags_with_whitespace.replace_all(s, "\n").into_owned()
+    let re = Regex::new(r"( *<p> *)").unwrap();
+    re.replace_all(s, "\n").into_owned()
 }
 
-fn remove_double_links(s: String) -> String {
-    "".to_owned()
+fn replace_link_opening_tags(s: &str) -> String {
+    let re = Regex::new(r"(<a+.*href=\x22.\x22*\s*?.*?\s*?> *)").unwrap();
+    re.replace_all(s, "").into_owned()
+}
+
+fn replace_link_closing_tags(s: &str) -> String {
+    let re = Regex::new(r"( *</a> *)").unwrap();
+    re.replace_all(s, "").into_owned()
+}
+
+fn replace_links(s: String) -> String {
+    let mut r = replace_link_opening_tags(&s);
+    r = replace_link_closing_tags(&r);
+    r
 }
 
 
@@ -40,9 +52,21 @@ mod test {
     fn test_replace_paragraphs() {
         let mut s: String = format!("aaaaaa zzz zzaaa <p> </p>");
         s = replace_paragraphs(s);
-        assert!(!s.contains("<p>"));
-        assert!(!s.contains("</p>"));
-        assert_eq!("aaaaaa zzz zzaaa", s);
+        assert_eq!("aaaaaa zzz zzaaa\n", s);
 
+        s = format!("<p> para</p> ");
+        s = replace_paragraphs(s);
+        assert_eq!("\npara", s);
     }
+
+
+    #[test]
+    fn test_replace_links() {
+        let mut s: String = format!("this is <a href=\"http\"://www.link.com rel=\"nofollow\"> \
+                                     http://www.link.com </a>");
+        s = replace_links(s);
+        assert_eq!("this is http://www.link.com", s);
+    }
+
+
 }
