ac73af4e9a537417b451aec43ad994d6f6973cbf
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-07-14 18:44:54 +0300

    Moved to regex

--- cli/src/formatting/tokens.rs
+++ cli/src/formatting/tokens.rs
@@ -10,10 +10,25 @@ pub enum FormatType {
     Other,
 }
 
-fn replace_paragraphs(s:String) -> String {
-    let mut r = s.replace("<p>", "\n");
-    r = r.replace("</p>", "");
+fn replace_paragraphs(s: String) -> String {
+    // todo split
+    let mut r = replace_paragraphs_opening_tags(&s);
+    r = replace_paragraphs_closing_tags(&r);
     r
+}
+
+fn replace_paragraphs_closing_tags(s: &str) -> String {
+    let closing_tags_with_whitespace = Regex::new(r"(\s*</p>\s*)").unwrap();
+    closing_tags_with_whitespace.replace_all(s, "").into_owned()
+}
+
+fn replace_paragraphs_opening_tags(s: &str) -> String {
+    let opening_tags_with_whitespace = Regex::new(r"(\s*<p>\s*)").unwrap();
+    opening_tags_with_whitespace.replace_all(s, "\n").into_owned()
+}
+
+fn remove_double_links(s: String) -> String {
+    "".to_owned()
 }
 
 
@@ -23,10 +38,11 @@ mod test {
 
     #[test]
     fn test_replace_paragraphs() {
-        let mut s:String = format!("aaaaaa zzz zzaaa <p> </p>");
+        let mut s: String = format!("aaaaaa zzz zzaaa <p> </p>");
         s = replace_paragraphs(s);
         assert!(!s.contains("<p>"));
         assert!(!s.contains("</p>"));
+        assert_eq!("aaaaaa zzz zzaaa", s);
 
     }
 }
