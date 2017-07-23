52b621222cc19cdce256411097b269b74048ac01
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-07-23 20:41:23 +0300

    Fixes the test and adds comment

--- cli/src/formatting/general_formatter.rs
+++ cli/src/formatting/general_formatter.rs
@@ -23,9 +23,8 @@ impl GeneralFormatter {
             } else {
                 characters_since_newline = 0;
             }
-            if characters_since_newline > line_len {
-                // todo this does not work well when there already exists newlines on the line, e.g when tag formatting replaces <p> with \n
-                result.push('\n');
+            if characters_since_newline >= line_len { // Currently since the plus is before it'll 
+                result.push('\n');                    // add newline if the line would be over the limit and then push the word
                 characters_since_newline = 0;
             }
             result.push_str(word);
@@ -54,7 +53,7 @@ mod test {
             *counts.entry(c).or_insert(0) += 1;
         }
         let count = counts.get(&'\n').unwrap();
-        assert_eq!(*count, s.len() / line_len);
+        assert_eq!(*count, (s.len() / line_len) - 1); // there's no newline at the end
         assert!(*count > 0);
 
     }
